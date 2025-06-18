33420de732524184846d41d1707eaf329684851c
Pierre Bermudez <bermudezpierre@gmail.com>
2025-06-18 17:39:41 +0200

    feat: create hero banner

--- front/app/routes/layout.tsx
+++ front/app/routes/layout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center">
-      <MenuNavigation />
-      <main className="flex flex-col items-center justify-center w-full">
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center md:items-stretch">
+      {/* <MenuNavigation /> */}
+      <main className="flex flex-col items-center justify-center w-full md:items-stretch">
         <Outlet />
       </main>
     </div>
