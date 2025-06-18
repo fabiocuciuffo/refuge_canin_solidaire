602aa069aa24e555fb43562bb0322a351529a1af
fabiocuciuffo <cuciuffofab@gmail.com>
2025-06-18 17:49:10 +0200

    button classes fix

--- front/app/components/Button.tsx
+++ front/app/components/Button.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router"
 function Button ({children, className, link}:{children: React.ReactNode, className?: string, link?: string}) {
   if (link) {
     return(
-    <Link to={link} className={"inline-block py-3 w-full text-center rounded-lg" + " " + className}>
+    <Link to={link} className={"inline-block py-3 w-full text-center rounded-lg text-regular text-heavy" + " " + className}>
       {children}
     </Link>
     )
   } else {
         return(
-    <button  className={"inline-block py-3 w-full text-center rounded-lg" + " " + className}>
+    <button  className={"inline-block py-3 w-full text-center rounded-lg text-regular text-heavy" + " " + className}>
       {children}
     </button>
     )
