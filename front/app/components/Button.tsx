8be670e0580abe34c5d3a2d743399ae9dafc073b
fabiocuciuffo <cuciuffofab@gmail.com>
2025-06-19 00:18:54 +0200

    nav button et assets

--- front/app/components/Button.tsx
+++ front/app/components/Button.tsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router"
 
-function Button ({children, className, link}:{children: React.ReactNode, className?: string, link?: string}) {
+function Button ({children, className, link, onClickCallback}: {children: React.ReactNode, className?: string, link?: string, onClickCallback?: (e: React.MouseEvent<HTMLButtonElement>) => void}) {
   if (link) {
     return(
-    <Link to={link} className={"inline-block py-3 w-full text-center rounded-lg text-regular text-heavy" + " " + className}>
-      {children}
-    </Link>
+      <Link to={link} className={"inline-block py-3 w-full text-center rounded-lg text-regular text-heavy" + " " + className}>
+        {children}
+      </Link>
     )
   } else {
-        return(
-    <button  className={"inline-block py-3 w-full text-center rounded-lg text-regular text-heavy" + " " + className}>
-      {children}
-    </button>
+    return(
+      <button onClick={onClickCallback} className={"inline-block py-3 w-full text-center rounded-lg text-regular text-heavy" + " " + className}>
+        {children}
+      </button>
     )
   }
 }
--- front/app/components/MenuNavigation.tsx
+++ front/app/components/MenuNavigation.tsx
@@ -1,53 +1,139 @@
+import { useRef } from "react";
 import { Link } from "react-router";
+import Button from "./Button"
 
 const MenuNavigation = () => {
+  const menu_mobile = useRef<HTMLDivElement | null>(null)
+  const menu_mobile_inside = useRef<HTMLDivElement | null>(null)
+  const menu_mobile_img = useRef<HTMLImageElement | null>(null)
+
+  function openMenu(e: React.MouseEvent<HTMLButtonElement>): void {
+    e.preventDefault();
+    if (menu_mobile.current && !menu_mobile.current.classList.contains("visible")) {
+      menu_mobile.current.classList.add("visible")
+      menu_mobile.current.classList.remove("-translate-y-full")
+      if (menu_mobile_img.current) {
+        menu_mobile_img.current.classList.add("opacity-0")
+        setTimeout(() => {
+          if (menu_mobile_img.current) {
+            menu_mobile_img.current.src = "/vector/cross.svg"
+            menu_mobile_img.current.classList.remove("opacity-0")
+          }
+        }, 100)
+      }
+      setTimeout(() => {
+        if (menu_mobile_inside.current) {
+          menu_mobile_inside.current.classList.remove("opacity-0")
+        }
+      }, 300)
+      document.body.style.overflow = "hidden"
+    } else if (menu_mobile.current && menu_mobile.current.classList.contains("visible")) {
+      if (menu_mobile.current && menu_mobile.current.classList.contains("visible")) {
+        document.body.style.overflow = ""
+        if (menu_mobile_img.current) {
+          menu_mobile_img.current.classList.add("opacity-0")
+          setTimeout(() => {
+            if (menu_mobile_img.current) {
+              menu_mobile_img.current.src = "/vector/burger_menu.svg"
+              menu_mobile_img.current.classList.remove("opacity-0")
+            }
+          }, 100)
+        }
+        if (menu_mobile_inside.current) {
+          menu_mobile_inside.current.classList.add("opacity-0")
+        }
+        setTimeout(() => {
+          if (menu_mobile.current) {
+            menu_mobile.current.classList.remove("visible")
+            menu_mobile.current.classList.add("-translate-y-full")
+          }
+        }, 300)
+      }
+    }
+  }
+
+  function toggleMobileCollapseLink(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    const content = e.currentTarget.nextElementSibling;
+    const chevron = e.currentTarget.querySelector("img");
+    if (content && chevron) {
+      if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden");
+        chevron.classList.add("rotate-90");
+      } else {
+        setTimeout(() => {
+          content.classList.add("hidden");
+          chevron.classList.remove("rotate-90");
+        }, 100)
+      }
+    }
+
+  }
+
   return (
-    <nav className="w-full bg-white shadow-md py-4 px-8 flex justify-between items-center rounded-none">
-      <Link to="/" className="text-2xl font-bold text-blue-700">
-        Refuge Canin Solidaire
-      </Link>
-      <ul className="flex space-x-8">
-        <li>
-          <Link
-            to="/"
-            className="text-gray-700 hover:text-blue-700 transition-colors font-medium"
-          >
-            Accueil
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/ressources"
-            className="text-gray-700 hover:text-blue-700 transition-colors font-medium"
-          >
-            Ressources
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/adoptions"
-            className="text-gray-700 hover:text-blue-700 transition-colors font-medium"
-          >
-            Adoption
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/a-propos"
-            className="text-gray-700 hover:text-blue-700 transition-colors font-medium"
-          >
-            À propos
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/contact"
-            className="text-gray-700 hover:text-blue-700 transition-colors font-medium"
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <nav className="fixed justify-between md:items-center w-full h-fit flex py-2 px-4 bg-white">
+      <div className="relative flex justify-between md:hidden w-full z-50">
+        <img src="/images/logo_RCS.svg" alt="" className="max-w-48" />
+        <div className="inline-flex">
+          <button onClick={openMenu} className="bg-blue p-4 rounded-lg w-12 flex justify-center items-center"><img src="/vector/burger_menu.svg" alt="" className="transition-all duration-100 ease-in-out" ref={menu_mobile_img} /></button>
+        </div>
+      </div>
+      <div className="invisible h-screen w-full bg-white absolute z-40 top-0 left-0 transition-all duration-300 ease-in-out  -translate-y-full flex flex-col justify-center md:hidden px-4" id="menu_mobile" ref={menu_mobile} >
+        <div className="w-full h-fit py-2">
+          <img src="/images/logo_RCS.svg" alt="" className="max-w-48 invisible" />
+        </div>
+        <div className="bg-beige flex flex-col w-full h-full rounded-2xl opacity-0 transition-all duration-300 ease-in-out justify-start p-4 my-4" ref={menu_mobile_inside}>
+          <div className="my-auto">
+            <Button link="/">J'adopte</Button>
+            <Button link="/">Le refuge</Button>
+            <Button link="/">Devenir bénévole</Button>
+            <Button link="/">Boutique</Button>
+            <div className="flex flex-col justify-center items-center">
+              <Button
+                onClickCallback={toggleMobileCollapseLink}
+                className="flex! items-center justify-center w-full"
+              >
+                Ressources
+                <img
+                  src="/vector/chevron.svg"
+                  alt="Chevron"
+                  className="transition-transform duration-300 ease-in-out"
+                />
+              </Button>
+              <div className="hidden flex-col px-auto transition-all duration-100 ease-in-out">
+                <Button link="/" className="p-0.5! text-regular-weight">Blog</Button>
+                <Button link="/" className="p-0.5! text-regular-weight">Événements</Button>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <Button link="/" className="border">
+              Contactez-nous
+            </Button>
+            <span className="invisible min-w-3"></span>
+            <Button link="/" className="bg-blue">
+              Faire un don
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="hidden md:flex justify-between w-full z-10">
+        <img src="/images/logo_RCS.svg" alt="" className="max-w-48 me-2" />
+        <div className="flex me-2">
+          <Button link="/" className="whitespace-nowrap me-6">J'adopte</Button>
+          <Button link="/" className="whitespace-nowrap me-6">Le refuge</Button>
+          <Button link="/" className="whitespace-nowrap me-6">Devenir bénévole</Button>
+          <Button link="/" className="whitespace-nowrap">Boutique</Button>
+        </div>
+        <div className="flex w-fit ">
+          <Button link="/" className="border whitespace-nowrap w-fit! px-6 me-2">
+            Contactez-nous
+          </Button>
+          <Button link="/" className="bg-blue whitespace-nowrap w-fit! px-6">
+            Faire un don
+          </Button>
+        </div>
+      </div>
     </nav>
   );
 };
