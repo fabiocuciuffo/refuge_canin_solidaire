d04225402f8761e01071094d9d0ad4c2d6de0958
fabiocuciuffo <cuciuffofab@gmail.com>
2025-06-18 17:44:13 +0200

    button component

---
+++ front/app/components/Button.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router"
+
+function Button ({children, className, link}:{children: React.ReactNode, className?: string, link?: string}) {
+  if (link) {
+    return(
+    <Link to={link} className={"inline-block py-3 w-full text-center rounded-lg" + " " + className}>
+      {children}
+    </Link>
+    )
+  } else {
+        return(
+    <button  className={"inline-block py-3 w-full text-center rounded-lg" + " " + className}>
+      {children}
+    </button>
+    )
+  }
+}
+
+export default Button
