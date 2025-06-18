db6a6101fabff696ccf2445177d96a90c6cf3da0
Pierre Bermudez <bermudezpierre@gmail.com>
2025-06-18 10:10:21 +0200

    feat: add category field to posts schemas

--- front/app/server/env.ts
+++ front/app/server/env.ts
@@ -1,8 +1,8 @@
 import "dotenv";
 
-const sanityId = process.env.REACT_APP_SANITY_PROJECT_ID;
-const sanityDataset = process.env.REACT_APP_SANITY_DATASET;
-const sanityToken = process.env.REACT_APP_SANITY_TOKEN;
+const sanityId = process.env.SANITY_PROJECT_ID;
+const sanityDataset = process.env.SANITY_DATASET;
+const sanityToken = process.env.SANITY_TOKEN;
 const gmailUser = process.env.GMAIL_USER;
 const gmailPassword = process.env.GMAIL_PASSWORD;
 const emailFrom = process.env.EMAIL_FROM;
