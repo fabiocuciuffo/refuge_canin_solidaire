abc00e2b45a8324ea0ab9e7cb0d4271093a8aa00
fabiocuciuffo <cuciuffofab@gmail.com>
2025-06-19 00:26:45 +0200

    retour de la nav dans le layout

--- front/app/routes/layout.tsx
+++ front/app/routes/layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 export default function Layout() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center md:items-stretch">
-      {/* <MenuNavigation /> */}
+      <MenuNavigation />
       <main className="flex flex-col items-center justify-center w-full md:items-stretch">
         <Outlet />
       </main>
